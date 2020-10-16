89d39aef66b6299bee1efd77304817f0b5833c72
Stud. Federico Bove <f.bove.0899@vallauri.net>
2020-10-16 11:06:38 +0200

    Modified

--- 001 - NodeJs/server.js
+++ 001 - NodeJs/server.js
@@ -33,8 +33,8 @@ var fs=require("fs");
 var app=require("http").createServer(mainHandler).listen(8080);
 console.log(__dirname);
 
-function mainHandler(req,res){
-    fs.readFile(__dirname+'/index.html',function(err,content){
+function mainHandler(req, res){
+    fs.readFile(__dirname + '/index.html', function(err, content){
         if(err){
             res.writeHead(500);
             return res.end('Error loading file');
