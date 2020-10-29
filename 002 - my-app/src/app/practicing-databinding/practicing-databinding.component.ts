24fc659e6710e8af245866eeb9affb79e533d15d
FedeBove <71231228+FedeBove@users.noreply.github.com>
2020-10-29 19:10:46 +0100

    Modified!

--- 002 - my-app/src/app/practicing-databinding/practicing-databinding.component.ts
+++ 002 - my-app/src/app/practicing-databinding/practicing-databinding.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PracticingDatabindingComponent implements OnInit {
 
+  allowSend:boolean = false;
+  username:string = "";
+  newUsername:string ='No Username was Created.';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onCreateUsername(){
+    this.newUsername = "Username Created: " + this.username + "!";  
+    this.username = "";
+    this.allowSend = false;
+  }
+
+  enableButton(){
+    if(this.username){
+      this.allowSend = true;
+    }
+  }
 }
