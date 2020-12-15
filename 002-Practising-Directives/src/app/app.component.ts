cb1c8a1f356a667e6b49b6daca021c2b2d0d6f8b
FedeBove <71231228+FedeBove@users.noreply.github.com>
2020-12-15 11:53:06 +0100

    Modified

---
+++ 002-Practising-Directives/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Angular Course: Practising-Directives';
+  isDetailsVisible:boolean=false;
+  toggleDetailsButtonLabel:string='Show Details';
+  logs:any=[];
+
+  onToggleDetailsClick(){
+    this.isDetailsVisible=!this.isDetailsVisible;
+    this.toggleDetailsButtonLabel=this.isDetailsVisible?'Hide Details':'Show Details';
+    this.logs.push(Date.now().toString());
+    console.log(this.logs);
+  }
+}
