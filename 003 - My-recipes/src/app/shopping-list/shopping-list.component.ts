db9a9ce10a48377c5db6e15b2d1b86c8d48ca27f
FedeBove <71231228+FedeBove@users.noreply.github.com>
2021-02-12 16:21:12 +0100

    Esercizio Aggiunto

---
+++ 003 - My-recipes/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Pancetta', 50),
+    new Ingredient('Pasta', 750)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
